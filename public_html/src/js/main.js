23d1585bd4c783a8e2586e1992c5926b7abc19aa
Aaron Sherrill <aaron@digitalsurgeons.com>
2015-04-06 08:10:47 -0400

    Opening on desktop

--- public_html/src/js/main.js
+++ public_html/src/js/main.js
@@ -2,10 +2,52 @@
 
     "use strict";
 
-    var enquire = require('/src/js/components/enquire.min.js');
+    //this module uses jquery, enquire, and slick.
+    var jQuery = require("../../bower_components/jquery/dist/jquery.min");
+    var enquire = require("../../bower_components/enquire");
+    require("../../bower_components/slick.js");
 
     document.addEventListener('DOMContentLoaded', function() {
-        console.log("Arccos Promo");
+
+        //On mobile and tablet the brand guidelines section will be a slickjs swiper
+        var $swiperElem = jQuery('[data-slickjs-wrapper]');
+        // enquire.register("screen and (max-width:767px)",{
+        //   match: function(){
+        //     $swiperElem.addClass("live");
+        //   },
+        //   unmatch: function(){
+        //     $swiperElem.removeClass("live");
+        //   }
+        // });
+
+        $swiperElem.slick({
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 3,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
+        });
+
+
+
     });
 
 })();
